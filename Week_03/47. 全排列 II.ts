bb331642892c0ad566585002b025355176a69391
jialuzhang <jialuzhang0805@gmail.com>
2020-06-08 00:04:27 +0800

    week3 作业提交

--- Week_03/47. 全排列 II.ts
+++ Week_03/47. 全排列 II.ts
@@ -1,26 +1,30 @@
-let allOrder: Map<number[],boolean>  = new Map();
+let allOrder: Map<string,number[]>  = new Map();
 let visted: boolean[] = [];
 function permuteUnique(nums: number[]): number[][] {
-    allOrder = [];
+    allOrder = new Map();
     visted = [];
     for(let i = 0;i < nums.length;i ++) {
         visted.push(false);
     }
     dfs(nums, 0, []);
     let ans: number[][] = []
-    for (let (key, value) of allOrder) {
-        ans.push(key);
+    for (let [key, value] of allOrder) {
+        // console.log(key);
+        ans.push(value);
     }
     return ans;
 };
 
 function dfs(nums: number[], level: number, order: number[]):void {
     if (level == nums.length) {
-        let t: number[] = [];
-        t = t.concat(order);
-        if (allOrder.get(t) == undefined) {
-            allList.set(t, true);
+        let t: string = "";
+        for (let i of order){
+            t += i;
         }
+        let tmp: number[] = [];
+        // if (allOrder.get(t) == undefined) {
+        allOrder.set(t, tmp.concat(order));
+        // }
         return;
     }
 
